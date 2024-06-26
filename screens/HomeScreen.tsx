a04c7127c0ea7a2163544090fee0a9bd5fc521af
Friedrich Commichau <f.commichau@hm.edu>
2024-06-26 19:47:38 +0200

    insert ScaleView

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -3,11 +3,12 @@ import React, { useState } from 'react';
 import { View, Text, StyleSheet, Button, SafeAreaView, Dimensions } from 'react-native';
 import FadeView from '../animations/FadeView';
 import SlideView from '../animations/SlideView';
-import { get } from 'react-native/Libraries/TurboModule/TurboModuleRegistry';
+import ScaleView from '../animations/ScaleView';
 
 const HomeScreen: React.FC = () => {
     const [visibility, setVisibility] = useState(false);
     const [slideVisibility, setSlideVisibility] = useState(false);
+    const [scaleVisibility, setScaleVisibility] = useState(false);
     
     const getScreenWidth = () => {
         return Math.round(Dimensions.get('window').width);
@@ -22,13 +23,19 @@ const HomeScreen: React.FC = () => {
             <View style={styles.buttonRow}>
                 <Button title="Toggle visibility!" onPress={() => setVisibility(prev => !prev)} />
             </View>
-            <View style={{ width: '100%', alignItems: 'flex-start'}} >    
+            <View style={styles.containerForSlide} >    
                 <SlideView style={styles.slidingContainer} visible={slideVisibility}>
                     <Text style={styles.fadingText}>Slide Content</Text>
                 </SlideView>
             </View>
             <View style={styles.buttonRow}>
-                <Button title="Slide sth. in!" onPress={() => setSlideVisibility(prev => !prev)} />
+                <Button title="Slide it!" onPress={() => setSlideVisibility(prev => !prev)} />
+            </View>
+            <ScaleView style={styles.scalingContainer} visible={scaleVisibility}>
+                <Text style={styles.scalingText}>Scale Content</Text>
+            </ScaleView>
+            <View style={styles.buttonRow}>
+                <Button title="Scale it!" onPress={() => setScaleVisibility(prev => !prev)} />
             </View>
             
         
@@ -37,34 +44,50 @@ const HomeScreen: React.FC = () => {
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: '100%',
-    height: '100%',
-  },
-  fadingContainer: {
-    padding: 16,
-    backgroundColor: 'powderblue',
-    borderRadius: 8,
-    marginBottom: 10,
-  },
-  fadingText: {
-    fontSize: 28,
-  },
-  slidingContainer: {
-    width: '50%',
-    alignItems: 'center',
-    backgroundColor: 'powderblue',
-    padding: 16,
-    borderRadius: 8,
-  },
-  buttonRow: {
-    flexBasis: 100,
-    justifyContent: 'space-evenly',
-    marginVertical: 16,
-  },
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      width: '100%',
+      height: '100%',
+    },
+    fadingContainer: {
+      padding: 16,
+      backgroundColor: 'powderblue',
+      borderRadius: 8,
+      marginBottom: 10,
+    },
+    fadingText: {
+      fontSize: 28,
+    },
+    containerForSlide: {
+        width: '100%',
+        alignItems: 'flex-start',
+        borderColor: '#f1f1f1',
+        borderWidth: 1,
+    },
+    slidingContainer: {
+      width: '50%',
+      alignItems: 'center',
+      backgroundColor: 'powderblue',
+      padding: 16,
+      borderRadius: 8,
+    },
+    buttonRow: {
+      flexBasis: 100,
+      justifyContent: 'space-evenly',
+      marginVertical: 16,
+    },
+    scalingContainer: {
+      width: '100%',
+      alignItems: 'center',
+      backgroundColor: 'powderblue',
+      padding: 16,
+      borderRadius: 8,
+    },
+    scalingText: {
+        fontSize: 28,
+    },
 });
 
 export default HomeScreen;
