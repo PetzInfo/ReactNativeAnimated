8993183452ffea080082e065e334c2c781a08ff6
Friedrich Commichau <f.commichau@hm.edu>
2024-06-26 13:10:04 +0200

    add FadeInView

--- animations/FadeInView.tsx
+++ animations/FadeInView.tsx
@@ -1,24 +1,29 @@
+// src/animations/FadeInView.tsx
 import React, { useRef, useEffect } from 'react';
-import { Animated, StyleProp, ViewProps, ViewStyle } from 'react-native';
+import { Animated, StyleProp, ViewStyle } from 'react-native';
 
-interface FadeInViewProps extends ViewProps {
+interface FadeInViewProps {
   duration?: number;
   style?: StyleProp<ViewStyle>;
+  children: React.ReactNode;
+  trigger: boolean;
 }
 
-const FadeInView: React.FC<FadeInViewProps> = ({ duration = 1000, children, style, ...props }) => {
+const FadeInView: React.FC<FadeInViewProps> = ({ duration = 1000, style, children, trigger }) => {
   const fadeAnim = useRef(new Animated.Value(0)).current;
 
   useEffect(() => {
-    Animated.timing(fadeAnim, {
-      toValue: 1,
-      duration: duration,
-      useNativeDriver: true,
-    }).start();
-  }, [fadeAnim, duration]);
+    if (trigger) {
+      Animated.timing(fadeAnim, {
+        toValue: 1,
+        duration: duration,
+        useNativeDriver: true,
+      }).start();
+    }
+  }, [trigger, fadeAnim, duration]);
 
   return (
-    <Animated.View style={[style, { opacity: fadeAnim }]} {...props}>
+    <Animated.View style={[style, { opacity: fadeAnim }]}>
       {children}
     </Animated.View>
   );
